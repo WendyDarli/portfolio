8dd648e099e718d57ee13f214118cf992915aaa2
wendydarli <139488528+WendyDarli@users.noreply.github.com>
2026-03-13 18:06:53 -0300

    feat: projects section carousel

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -2,11 +2,26 @@ import './ProjectsSection.css';
 
 function ProjectsSection(){
 
+    const items = [
+        'React','➢','JavaScript','➢','JWT','➢','Node.js','➢',
+        'Express','➢','Restful APIs','➢','ORMs','➢','Docker','➢','Caching','➢'
+    ];
+
     return(
         <section id='projects' className='section-container'>      
-            <div>
-                <p className='shadow-text'>Projects &lt;3</p>
+            <div className='tech-carousel'>
+                <div className='track'>              
+                    {[...items].map((text, index) => (
+                        <p key={index}>{text}</p>
+                    ))}
+                </div>
+                <div className='track'>              
+                    {[...items].map((text, index) => (
+                        <p key={index + '#'}>{text}</p>
+                    ))}
+                </div>
             </div>
+            <p className='shadow-text'>Projects &lt;3</p>
         </section>
 
     );
