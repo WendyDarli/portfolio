56ba674c000ef8a0db316862e2d49092914140b3
wendydarli <139488528+WendyDarli@users.noreply.github.com>
2026-04-17 09:37:47 -0300

    feat: hide sidebar on mobile

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -4,8 +4,8 @@ function SideBar({ activeSection }){
     return(
         <div className='side-header-container'>
             <a href='#contact' className={activeSection === 'contact' ? 'active' : ''}>contact</a>
-            <a href='#skills' className={activeSection === 'skills' ? 'active' : ''}>skills</a>
             <a href='#projects' className={activeSection === 'projects' ? 'active' : ''}>projects</a>
+            <a href='#skills' className={activeSection === 'skills' ? 'active' : ''}>skills</a>
             <a href='#about' className={activeSection === 'about' ? 'active' : ''}>about</a>
             <a href='#start' className={activeSection === 'start' ? 'active' : ''}>start</a>
         </div>
